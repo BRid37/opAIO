fee23d43224afb33b2743e03303d248268268b32
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-12-11 11:57:53 -0700

    Acceleration profiles
    Added toggle to use DragonPilot's acceleration profiles.
    
    Credit goes to DragonPilot!
    
    https: //github.com/dragonpilot-community/dragonpilot
    Co-Authored-By: eFini <16603033+efinilan@users.noreply.github.com>
    Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -210,6 +210,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"WheeledBody", PERSISTENT},
 
     // FrogPilot parameters
+    {"AccelerationProfile", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
