5efb16579fea399f11d8e6663c574ddc82beba69
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-12 22:39:30 -0700

    Allow device to be offline indefinitely
    Added button to allow the device to be offline indefinitely.

--- common/params.cc
+++ common/params.cc
@@ -219,6 +219,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"GasRegenCmd", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
+    {"OfflineMode", PERSISTENT},
     {"Updated", PERSISTENT},
 };
 
