5b9280c3940f0b7e82fafca265a62f1ea39d7dbe
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-12 22:39:30 -0700

    Disable resume required event after it's been shown

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -94,6 +94,9 @@ class CarInterfaceBase(ABC):
     self.belowSteerSpeed_shown = False
     self.disable_belowSteerSpeed = False
 
+    self.resumeRequired_shown = False
+    self.disable_resumeRequired = False
+
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed, sport_plus):
     if sport_plus:
