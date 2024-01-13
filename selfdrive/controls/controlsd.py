d237d5ca4626d89e3c4e1f0159a7c79e59aa8a0f
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-12 22:39:30 -0700

    Increase max speed by 5 on short press
    Added function to increase the value of the max speed by 5 instead of 1 by taping the "Max" speed icon in the onroad UI.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -513,7 +513,7 @@ class Controls:
   def state_transition(self, CS):
     """Compute conditional state transitions and execute actions on state transitions"""
 
-    self.v_cruise_helper.update_v_cruise(CS, self.enabled, self.is_metric)
+    self.v_cruise_helper.update_v_cruise(CS, self.enabled, self.is_metric, self.reverse_cruise_increase)
 
     # decrement the soft disable timer at every step, as it's reset on
     # entrance in SOFT_DISABLING state
@@ -979,6 +979,8 @@ class Controls:
 
     longitudinal_tune = self.params.get_bool("LongitudinalTune")
     self.sport_plus = self.params.get_int("AccelerationProfile") == 3 and longitudinal_tune
+
+    self.reverse_cruise_increase = self.params.get_bool("ReverseCruise")
 
 def main():
   controls = Controls()
