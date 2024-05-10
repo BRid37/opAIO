3bdc101bef480ae2d6ce37bdd9703619044bc869
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-09 22:20:54 -0700

    FrogPilot features - Error log button

--- selfdrive/ui/qt/offroad/software_settings.cc
+++ selfdrive/ui/qt/offroad/software_settings.cc
@@ -94,6 +94,14 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : ListWidget(parent) {
   });
   addItem(uninstallBtn);
 
+  // error log button
+  auto errorLogBtn = new ButtonControl(tr("Error Log"), tr("VIEW"), tr("View the error log for openpilot crashes."));
+  connect(errorLogBtn, &ButtonControl::clicked, [=]() {
+    const std::string txt = util::read_file("/data/community/crashes/error.txt");
+    ConfirmationDialog::rich(QString::fromStdString(txt), this);
+  });
+  addItem(errorLogBtn);
+
   fs_watch = new ParamWatcher(this);
   QObject::connect(fs_watch, &ParamWatcher::paramChanged, [=](const QString &param_name, const QString &param_value) {
     updateLabels();
