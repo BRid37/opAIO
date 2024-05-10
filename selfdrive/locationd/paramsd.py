6f0fcb014087c9944069498fa17232323518eed8
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-09 22:33:51 -0700

    FrogPilot features - Remove Global Subarus from Dashcam Mode

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -233,7 +233,7 @@ def main():
         0.2 <= liveParameters.stiffnessFactor <= 5.0,
         min_sr <= liveParameters.steerRatio <= max_sr,
       ))
-      if CP.carFingerprint == "RAM_HD":
+      if CP.carFingerprint == "RAM_HD" or CP.carName == "subaru" and CP.lateralTuning.which() == "torque":
         liveParameters.valid = True
       liveParameters.steerRatioStd = float(P[States.STEER_RATIO].item())
       liveParameters.stiffnessFactorStd = float(P[States.STIFFNESS].item())
