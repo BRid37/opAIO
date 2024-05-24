e26d2c0ac422ad44f7808f5789ea6e31ab90e7a3
CHaucke89 <132518562+CHaucke89@users.noreply.github.com>
2024-05-24 16:49:20 -0400

    opweb

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -93,6 +93,10 @@ procs = [
   PythonProcess("uploader", "system.loggerd.uploader", allow_uploads),
   PythonProcess("statsd", "system.statsd", allow_logging),
 
+  # PFEIFER - OPWEBGO {{
+  PythonProcess("opwebd", "selfdrive.opwebd", always_run),
+  # }} PFEIFER - OPWEBGO
+
   # debug procs
   NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
   PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
