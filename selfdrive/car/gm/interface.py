501f9c13da4677f69f774315f612da9a01269363
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-06 11:36:38 -0700

    FrogPilot community - Add 2023 Chevy Malibu No ACC
    Co-Authored-By: garrettpall <76917194+garrettpall@users.noreply.github.com>

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -248,6 +248,10 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.CADILLAC_CT6_CC:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
+    elif candidate == CAR.CHEVROLET_MALIBU_CC:
+      ret.steerActuatorDelay = 0.2
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+
     if ret.enableGasInterceptor:
       ret.networkLocation = NetworkLocation.fwdCamera
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM
