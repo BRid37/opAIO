f7c9bd9201a319b7b29c509ea54b3f341a5b2b27
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-07 03:05:56 -0700

    FrogPilot setup - Add more conversions

--- common/util.h
+++ common/util.h
@@ -36,6 +36,9 @@ const double MS_TO_KPH = 3.6;
 const double MS_TO_MPH = MS_TO_KPH * KM_TO_MILE;
 const double METER_TO_MILE = KM_TO_MILE / 1000.0;
 const double METER_TO_FOOT = 3.28084;
+const double FOOT_TO_METER = 1. / METER_TO_FOOT;
+const double CM_TO_INCH = 1. / 2.54;
+const double INCH_TO_CM = 1. / CM_TO_INCH;
 
 namespace util {
 
