3a34ccc5da810d70050746e471a4a5a6b288551a
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-10 06:39:00 -0700

    Vehicles - Toyota - Automatically Lock/Unlock Doors
    Automatically lock the doors when in drive and unlock when in park.
    
    Credit goes to AlexandreSato!
    
    https: //github.com/AlexandreSato/openpilot
    Co-Authored-By: Alexandre Nobuharu Sato <66435071+alexandresato@users.noreply.github.com>

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -51,6 +51,7 @@ const int TOYOTA_GAS_INTERCEPTOR_THRSLD = 805;
 // Stock longitudinal
 #define TOYOTA_COMMON_TX_MSGS                                                                                     \
   {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8},  /* LKAS + LTA + ACC & PCM cancel cmds */  \
+  {0x750, 0, 8},  /* white list 0x750 for Enhanced Diagnostic Request */                                                \
 
 #define TOYOTA_COMMON_LONG_TX_MSGS                                                                                                          \
   TOYOTA_COMMON_TX_MSGS                                                                                                                     \
@@ -362,7 +363,9 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   if (addr == 0x750) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
     bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U);
-    if (invalid_uds_msg) {
+    // AleSato added some more hack'sss
+    bool valid_uds_msgs = (GET_BYTES(to_send, 0, 4) == 0x11300540U);  // automatic door locking and unlocking
+    if (invalid_uds_msg && !valid_uds_msgs) {
       tx = 0;
     }
   }
