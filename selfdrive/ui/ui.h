2a0f51f04d5e92375959ac158b05eee11f2b0c7f
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-12 16:44:50 -0700

    Visuals - Model UI - Path Edges
    Adjust the width of the path edges shown on your UI to represent different driving modes and statuses.
    
    Default is 20% of the total path.
    
    Blue = Navigation
    Light Blue = 'Always On Lateral'
    Green = Default
    Orange = 'Experimental Mode'
    Red = 'Traffic Mode'
    Yellow = 'Conditional Experimental Mode' Overriden

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -209,6 +209,7 @@ typedef struct UIScene {
   float lane_width_right;
   float lat_accel;
   float lead_detection_threshold;
+  float path_edge_width;
   float speed_jerk;
   float speed_jerk_difference;
   float speed_limit;
@@ -237,6 +238,7 @@ typedef struct UIScene {
   int wheel_icon;
 
   QPolygonF track_adjacent_vertices[6];
+  QPolygonF track_edge_vertices;
 
 } UIScene;
 
