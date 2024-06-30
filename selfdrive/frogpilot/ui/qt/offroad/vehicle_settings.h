486c272aca609176b65f83ecc6e386ed83a606b8
BRid37 <55640145+BRid37@users.noreply.github.com>
2024-06-30 17:37:00 -0500

    Revert "Compile FrogPilot"
    This reverts commit 63a9440d57fdf837c2e502c02acd1bc9e2c8c5fd.

---
+++ selfdrive/frogpilot/ui/qt/offroad/vehicle_settings.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <set>
+
+#include <QStringList>
+
+#include "selfdrive/ui/qt/offroad/settings.h"
+#include "selfdrive/ui/ui.h"
+
+class FrogPilotVehiclesPanel : public FrogPilotListWidget {
+  Q_OBJECT
+
+public:
+  explicit FrogPilotVehiclesPanel(SettingsWindow *parent);
+
+private:
+  void hideToggles();
+  void setModels();
+  void updateCarToggles();
+  void updateState(const UIState &s);
+
+  ButtonControl *selectMakeButton;
+  ButtonControl *selectModelButton;
+
+  ToggleControl *disableOpenpilotLong;
+
+  QString carMake;
+  QString carModel;
+
+  QStringList models;
+
+  QMap<QString, QString> carModels;
+
+  std::set<QString> gmKeys = {"LongPitch", "VoltSNG"};
+  std::set<QString> subaruKeys = {"CrosstrekTorque"};
+  std::set<QString> toyotaKeys = {"ClusterOffset", "SNGHack", "ToyotaDoors", "ToyotaTune"};
+
+  std::map<std::string, AbstractControl*> toggles;
+
+  Params params;
+
+  bool hasExperimentalOpenpilotLongitudinal;
+  bool hasOpenpilotLongitudinal;
+  bool hasSNG;
+  bool isGMPCMCruise;
+  bool isImpreza;
+  bool started;
+};
---
+++ selfdrive/ui/qt/widgets/scrollview.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QScrollArea>
+
+class ScrollView : public QScrollArea {
+  Q_OBJECT
+
+public:
+  explicit ScrollView(QWidget *w = nullptr, QWidget *parent = nullptr);
+
+  // FrogPilot functions
+  void restorePosition(int previousScrollPosition);
+
+protected:
+  void hideEvent(QHideEvent *e) override;
+};
