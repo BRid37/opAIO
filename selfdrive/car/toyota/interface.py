138e627e95c284110f18cf4aa75666081c317590
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-07-22 23:31:29 -0700

    Vehicles - Toyota - Longitudinal Tune - FrogPilot

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -136,14 +136,18 @@ class CarInterface(CarInterfaceBase):
     ret.minEnableSpeed = -1. if (candidate in STOP_AND_GO_CAR or ret.enableGasInterceptor) else MIN_ACC_SPEED
 
     tune = ret.longitudinalTuning
-    if params.get_bool("CydiaTune"):
+    if params.get_bool("CydiaTune") or params.get_bool("FrogsGoMooTune"):
       ret.stopAccel = -2.5             # on stock Toyota this is -2.5
       ret.stoppingDecelRate = 0.3      # reach stopping target smoothly
       if candidate in TSS2_CAR or ret.enableGasInterceptor:
         tune.kpV = [0.0]
         tune.kiV = [0.5]
-        ret.vEgoStopping = 0.25
-        ret.vEgoStarting = 0.25
+        if params.get_bool("FrogsGoMooTune"):
+          ret.vEgoStopping = 0.15
+          ret.vEgoStarting = 0.15
+        else:
+          ret.vEgoStopping = 0.25
+          ret.vEgoStarting = 0.25
       else:
         tune.kpV = [0.0]
         tune.kiV = [1.2]               # appears to produce minimal oscillation on TSS-P
