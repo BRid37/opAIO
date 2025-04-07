6a9552d80044ffd472357d823a6685436408e9b7
multikyd <72329880+multikyd@users.noreply.github.com>
2025-04-07 15:55:43 +0900

    chksum

--- opendbc_repo/opendbc/safety/safety/safety_hyundai_canfd.h
+++ opendbc_repo/opendbc/safety/safety/safety_hyundai_canfd.h
@@ -25,13 +25,13 @@
 
 // *** Addresses checked in rx hook ***
 // EV, ICE, HYBRID: ACCELERATOR (0x35), ACCELERATOR_BRAKE_ALT (0x100), ACCELERATOR_ALT (0x105)
-#define HYUNDAI_CANFD_COMMON_RX_CHECKS(pt_bus)                                                               \
-  {.msg = {{0x35, (pt_bus), 32, .max_counter = 0xffU, .frequency = 100U},                                    \
-           {0x100, (pt_bus), 32, .max_counter = 0xffU, .frequency = 100U},                                   \
-           {0x105, (pt_bus), 32, .max_counter = 0xffU, .frequency = 100U}}},                                 \
-  {.msg = {{0x175, (pt_bus), 24, .max_counter = 0xffU, .frequency = 50U}, { 0 }, { 0 }}},                    \
-  {.msg = {{0xa0, (pt_bus), 24, .max_counter = 0xffU, .frequency = 100U}, { 0 }, { 0 }}},                    \
-  {.msg = {{0xea, (pt_bus), 24, .max_counter = 0xffU, .frequency = 100U}, { 0 }, { 0 }}},                    \
+#define HYUNDAI_CANFD_COMMON_RX_CHECKS(pt_bus)                                                                      \
+  {.msg = {{0x35, (pt_bus), 32, .ignore_checksum = true, .max_counter = 0xffU, .frequency = 100U},                  \
+           {0x100, (pt_bus), 32, .max_counter = 0xffU, .frequency = 100U},                                          \
+           {0x105, (pt_bus), 32, .max_counter = 0xffU, .frequency = 100U}}},                                        \
+  {.msg = {{0x175, (pt_bus), 24, .max_counter = 0xffU, .frequency = 50U}, { 0 }, { 0 }}},                           \
+  {.msg = {{0xa0, (pt_bus), 24, .max_counter = 0xffU, .frequency = 100U}, { 0 }, { 0 }}},                           \
+  {.msg = {{0xea, (pt_bus), 24, .ignore_checksum = true, .max_counter = 0xffU, .frequency = 100U}, { 0 }, { 0 }}},  \
 
 #define HYUNDAI_CANFD_STD_BUTTONS_RX_CHECKS(pt_bus)                                                               \
   HYUNDAI_CANFD_COMMON_RX_CHECKS(pt_bus)                                                                          \
