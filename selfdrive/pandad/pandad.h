db3a0e47d5ef53550c7f3be21d67f0d2e9c27e50
FrogAi <FrogAi@users.noreply.github.com>
2025-06-20 14:13:12 -0700

    Compile FrogPilot

--- selfdrive/pandad/pandad.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-#include "selfdrive/pandad/panda.h"
-
-bool safety_setter_thread(std::vector<Panda *> pandas);
-void pandad_main_thread(std::vector<std::string> serials);
