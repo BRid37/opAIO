338c97b23b1601b00a0b9e87d58490f524c743fb
James <91348155+FrogAi@users.noreply.github.com>
2025-10-04 11:13:39 -0700

    Detect if the driver is still in the car

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -17,6 +17,8 @@ def dmonitoringd_thread():
   # FrogPilot variables
   sm = sm.extend(['frogpilotCarState'])
 
+  driver_view_enabled = params.get_bool("IsDriverViewEnabled")
+
   # 20Hz <- dmonitoringmodeld
   while True:
     sm.update()
@@ -27,9 +29,11 @@ def dmonitoringd_thread():
     valid = sm.all_checks()
     if valid:
       DM.run_step(sm)
+    elif driver_view_enabled:
+      DM.face_detected = sm['driverStateV2'].leftDriverData.faceProb > DM.settings._FACE_THRESHOLD or sm['driverStateV2'].rightDriverData.faceProb > DM.settings._FACE_THRESHOLD
 
     # publish
-    dat = DM.get_state_packet(valid=valid)
+    dat = DM.get_state_packet(valid=valid or driver_view_enabled)
     pm.send('driverMonitoringState', dat)
 
     # load live always-on toggle
