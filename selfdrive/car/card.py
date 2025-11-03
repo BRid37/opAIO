858bcf8855195e94513307da74525295a74aaed8
James <91348155+FrogAi@users.noreply.github.com>
2025-11-03 01:39:56 -0700

    Remove "dashcamOnly" flag check

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -121,8 +121,8 @@ class Car:
 
     self.CP.alternativeExperience = 0
     openpilot_enabled_toggle = self.params.get_bool("OpenpilotEnabledToggle")
-    controller_available = self.CI.CC is not None and openpilot_enabled_toggle and not self.CP.dashcamOnly
-    self.CP.passive = not controller_available or self.CP.dashcamOnly
+    controller_available = self.CI.CC is not None and openpilot_enabled_toggle
+    self.CP.passive = not controller_available
     if self.CP.passive:
       safety_config = structs.CarParams.SafetyConfig()
       safety_config.safetyModel = structs.CarParams.SafetyModel.noOutput
