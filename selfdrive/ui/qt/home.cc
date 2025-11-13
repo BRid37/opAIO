killinen/openpilot

Retain the sidebar's position

selfdrive/ui/qt/home.cc
@@ -84,7 +84,7 @@ void HomeWindow::offroadTransition(bool offroad) {
   QJsonObject &frogpilot_toggles = frogpilot_scene.frogpilot_toggles;
 
   body->setEnabled(false);
-  sidebar->setVisible(offroad || frogpilot_toggles.value("debug_mode").toBool());
+  sidebar->setVisible(offroad || params.getBool("Sidebar") || frogpilot_toggles.value("debug_mode").toBool());
   if (offroad) {
     slayout->setCurrentWidget(home);
 
@@ -112,6 +112,7 @@ void HomeWindow::mousePressEvent(QMouseEvent* e) {
   // Handle sidebar collapsing
   if ((onroad->isVisible() || body->isVisible()) && (!sidebar->isVisible() || e->x() > sidebar->width())) {
     sidebar->setVisible(!sidebar->isVisible());
+    params.putBool("Sidebar", sidebar->isVisible());
   }
 }
 

selfdrive/ui/qt/window.cc
@@ -74,7 +74,7 @@ void MainWindow::closeSettings() {
   main_layout->setCurrentWidget(homeWindow);
 
   if (uiState()->scene.started) {
-    homeWindow->showSidebar(frogpilotUIState()->frogpilot_scene.frogpilot_toggles.value("debug_mode").toBool());
+    homeWindow->showSidebar(params.getBool("Sidebar") || frogpilotUIState()->frogpilot_scene.frogpilot_toggles.value("debug_mode").toBool());
   }
 }
 

BRid37/opAIO

selfdrive/ui/qt/home.cc
@@ -83,7 +83,7 @@ void HomeWindow::offroadTransition(bool offroad) {
   QJsonObject &frogpilot_toggles = frogpilot_scene.frogpilot_toggles;
 
   body->setEnabled(false);
-  sidebar->setVisible(offroad || frogpilot_toggles.value("debug_mode").toBool());
+  sidebar->setVisible(offroad || params.getBool("SidebarOpen") || frogpilot_toggles.value("debug_mode").toBool());
   if (offroad) {
     slayout->setCurrentWidget(home);
 
@@ -111,6 +111,7 @@ void HomeWindow::mousePressEvent(QMouseEvent* e) {
   // Handle sidebar collapsing
   if ((onroad->isVisible() || body->isVisible()) && (!sidebar->isVisible() || e->x() > sidebar->width())) {
     sidebar->setVisible(!sidebar->isVisible());
+    params.putBool("SidebarOpen", sidebar->isVisible());
   }
 }
 

selfdrive/ui/qt/window.cc
@@ -74,7 +74,7 @@ void MainWindow::closeSettings() {
   main_layout->setCurrentWidget(homeWindow);
 
   if (uiState()->scene.started) {
-    homeWindow->showSidebar(frogpilotUIState()->frogpilot_scene.frogpilot_toggles.value("debug_mode").toBool());
+    homeWindow->showSidebar(params.getBool("SidebarOpen") || frogpilotUIState()->frogpilot_scene.frogpilot_toggles.value("debug_mode").toBool());
   }
 }
 