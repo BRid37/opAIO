c7cca7a84e2a30512fc4a5e7529ed9f5cf713896
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    Pause steering with an active turn signal

--- frogpilot/controls/frogpilot_planner.py
+++ frogpilot/controls/frogpilot_planner.py
@@ -94,6 +94,7 @@ class FrogPilotPlanner:
     self.lateral_acceleration = v_ego**2 * sm["controlsState"].curvature
 
     self.lateral_check = v_ego >= frogpilot_toggles.pause_lateral_below_speed
+    self.lateral_check |= not (sm["carState"].leftBlinker or sm["carState"].rightBlinker) and frogpilot_toggles.pause_lateral_below_signal
     self.lateral_check |= sm["carState"].standstill
     self.lateral_check &= not sm["frogpilotCarState"].pauseLateral
 
