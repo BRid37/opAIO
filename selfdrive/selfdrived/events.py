47fbafc8a7832879d0599f6fa64efb84c32b883e
James <91348155+FrogAi@users.noreply.github.com>
2025-12-01 12:00:00 -0700

    Loud "Car Detected in Blindspot" Alert

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -1114,6 +1114,14 @@ FROGPILOT_EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
     ET.PERMANENT: holiday_alert,
   },
 
+  FrogPilotEventName.laneChangeBlockedLoud: {
+    ET.WARNING: Alert(
+      "Car Detected in Blindspot",
+      "",
+      AlertStatus.userPrompt, AlertSize.small,
+      Priority.LOW, VisualAlert.none, AudibleAlert.warningSoft, .1),
+  },
+
   FrogPilotEventName.leadDeparting: {
     ET.PERMANENT: Alert(
       "Lead Departed",
