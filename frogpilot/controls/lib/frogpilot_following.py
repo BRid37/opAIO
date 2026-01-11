a085c421b0bf1282646c6d4dd2e02b9ffcb66847
James <91348155+FrogAi@users.noreply.github.com>
2026-01-10 20:30:48 -0700

    Temp disable increasing "danger_factor"

--- frogpilot/controls/lib/frogpilot_following.py
+++ frogpilot/controls/lib/frogpilot_following.py
@@ -95,5 +95,5 @@ class FrogPilotFollowing:
         far_lead_offset = max(lead_distance - (v_ego * self.t_follow) - STOP_DISTANCE, 0)
         braking_offset += far_lead_offset
 
-      self.danger_factor += ((v_ego - v_lead) / 100)
+      #self.danger_factor += ((v_ego - v_lead) / 100)
       self.t_follow /= braking_offset
